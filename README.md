dbdc5ad1841d3da6965b9330e92857f27797dae0
Dante_Berishaj <db47425@ubt-uni.net>
2021-11-24 17:50:11 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # ZHDU--Vue-App
+
+cd server
+npm i
+
+cd..
+
+cd client/zhdu_app
+npm i 
+npm run serve
---
+++ client/zhdu_app/src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="home">
+    <img alt="Our logo" src="../assets/logo.png">
+    <HelloWorld msg="OUR Initial Project"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
