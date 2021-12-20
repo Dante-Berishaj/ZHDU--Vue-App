532903da9c5e34382ca3cef5c8bc3ff1f393b5bb
Bleart Nuredini <nuredini.bleart@gmail.com>
2021-12-20 11:55:14 +0100

    setup routing with vue-router

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -1,11 +1,16 @@
 <template>
-  <div class="nav">
-    <div class="nav-item">Home</div>
-    <div class="nav-item">Cities</div>
-    <div class="nav-item">Places</div>
-    <div class="nav-item">About</div>
-  </div>
+  <header>
+    <img alt="Vue logo" src="../assets/logo.png" height="100">
+    <nav>
+      <ul>
+        <li><router-link to="/">Home</router-link></li>
+        <li><router-link to="/cities">Cities</router-link></li>
+        <li><router-link to="/about">About</router-link></li>
+      </ul>
+    </nav>
+  </header>
 </template>
+
 
 <script>
 export default {
@@ -14,18 +19,26 @@ export default {
 </script>
 
 <style>
-.nav {
+header {
   display: flex;
-  flex-direction: row;
-  justify-content: flex-end;
-  margin-bottom: 1rem;
+  align-items: center;
+  border-bottom: 0.1rem solid #ccc;
 }
 
-.nav-item {
-  padding: 1.8rem;
+header nav {
+  margin-left: auto;
 }
 
-.nav-item:hover {
-  cursor: pointer;
+header nav ul {
+  list-style: none;
+}
+
+header nav ul li {
+  display: inline-flex;
+  margin-left: 1rem;
+}
+
+header a {
+  text-decoration: none;
 }
 </style>
--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to OUR Initial Vue.js App"/>
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import HomeTitle from '../components/HomeTitle.vue';
 
 export default {
   name: 'Home',
   components: {
-    HelloWorld
+    HomeTitle
   }
 }
 </script>
