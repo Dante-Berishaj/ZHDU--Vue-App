85192152e653b1144c6314d5b2a4c9418bf8b6ee
Dante_Berishaj <db47425@ubt-uni.net>
2021-12-20 12:27:14 +0100

    testing page

--- client/src/components/NavBar.vue
+++ client/src/components/NavBar.vue
@@ -6,6 +6,7 @@
         <li><router-link to="/">Home</router-link></li>
         <li><router-link to="/cities">Cities</router-link></li>
         <li><router-link to="/about">About</router-link></li>
+        <li><router-link to="/tests">Test</router-link></li>
       </ul>
     </nav>
   </header>
--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Cities from '../views/Cities.vue'
+import Test from '../views/Test.vue'
 
 Vue.use(VueRouter)
 
@@ -23,6 +24,11 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/tests',
+    name: 'Test',
+    component: Test
   }
 ]
 
