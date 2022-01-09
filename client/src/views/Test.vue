903ed690cc685e8c5094794fe61057ec40d87303
Dante_Berishaj <db47425@ubt-uni.net>
2022-01-09 19:29:15 +0100

    console logging data from the hotel api

--- client/src/views/Test.vue
+++ client/src/views/Test.vue
@@ -1,13 +1,24 @@
 <template>
-  <div class="test">
-    <h1>Test</h1>
+  <div>
+    <h2>Test</h2>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import axios from 'axios';
+import VueAxios from 'vue-axios';
+
+Vue.use(VueAxios, axios)
 
 export default {
   name: 'Test',
+  mounted() {
+    Vue.axios.get('http://localhost:5001/api/hotel')
+    .then((res) => {
+      console.warn(res)
+    });
+  },
 }
 
 
