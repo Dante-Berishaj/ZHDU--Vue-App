7b0bf7d3b886d0fa1c5d2f92b529f741aa5c3d29
Dante_Berishaj <db47425@ubt-uni.net>
2022-01-09 19:50:24 +0100

    showing  data from api using horrible styles

--- client/src/views/Test.vue
+++ client/src/views/Test.vue
@@ -1,6 +1,13 @@
 <template>
   <div>
     <h2>Test</h2>
+    <table border="1px">
+      <tr v-for="item in list" v-bind:key="item._id">
+        <td>{{item.title}}</td>
+        <td>{{item.category}}</td>
+        <td>{{item.content}}</td>
+      </tr>
+    </table>
   </div>
 </template>
 
@@ -13,10 +20,14 @@ Vue.use(VueAxios, axios)
 
 export default {
   name: 'Test',
+  data(){
+    return {list: undefined}
+  },
   mounted() {
     Vue.axios.get('http://localhost:5001/api/hotel')
     .then((res) => {
-      console.warn(res)
+      this.list = res.data;
+      console.warn(res.data)
     });
   },
 }
