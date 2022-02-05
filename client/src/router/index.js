3629569e6041740701055bcf6735a404695281fc
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2022-02-05 04:00:44 +0100

    city details page
    now every city has a details page with its hotels and attractions

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -2,9 +2,13 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
 import Cities from '../views/Cities.vue'
-import Test from '../views/Test.vue'
 import Hotels from '../views/Hotels.vue'
 import Attractions from '../views/Attractions.vue'
+import Prishtina from '../views/cityDetails/Prishtina'
+import Prizreni from '../views/cityDetails/Prizreni'
+import Peja from '../views/cityDetails/Peja'
+import Mitrovica from '../views/cityDetails/Mitrovica'
+import Gjakova from '../views/cityDetails/Gjakova'
 
 Vue.use(VueRouter)
 
@@ -20,6 +24,31 @@ const routes = [
     component: Cities
   },
   {
+    path: '/prishtina',
+    name: 'Prishtina',
+    component: Prishtina
+  },
+   {
+    path: '/prizreni',
+    name: 'Prizreni',
+    component: Prizreni
+  },
+  {
+    path: '/peja',
+    name: 'peja',
+    component: Peja
+  },
+  {
+    path: '/gjakova',
+    name: 'Gjakova',
+    component: Gjakova
+  },
+  {
+    path: '/mitrovica',
+    name: 'Mitrovica',
+    component: Mitrovica
+  },
+  {
     path: '/attractions',
     name: 'Attractions',
     component: Attractions
@@ -31,11 +60,6 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  },
-  {
-    path: '/tests',
-    name: 'Test',
-    component: Test
   },
   {
     path: '/hotels',
