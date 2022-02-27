8841acf73dbddc2a8fba32b3c547c2ba41cf7ef4
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2022-02-27 01:05:51 +0100

    adding restaurants and hotels in homepage with real data

--- client/src/components/LatestRestaurants.vue
+++ client/src/components/LatestRestaurants.vue
@@ -1,36 +1,36 @@
 <template>
-  <div class="block txt">
+ <div class="block txt">
     <v-container>
-      <h2 class="text-center">Accommodation</h2>
+      <h2 class="text-center">Restaurants</h2>
       <v-row>
-        <v-col  v-for="hotel in hotels.slice(0,3)" :key="hotel._id" cols="12" md="4">
+        <v-col cols="12" sm="4" v-for="restaurant in restaurants.slice(0,3)" v-bind:key="restaurant._id">
           <v-card dark outlined class="mx-auto"
-           :to="{ name: 'hotelDetails', params: { id: hotel._id } }">
+           :to="{ name: 'restaurantDetails', params: { id: restaurant._id } }">
               <v-img
                class="align-end"
                height="200px"    
-              :src="require(`../../../server/uploads/${hotel.image}`)">
+              :src="require(`../../../server/uploads/${restaurant.image}`)">
             </v-img>
-            <v-card-title>{{ hotel.title }}
+            <v-card-title>{{ restaurant.title }}
               <v-btn right absolute class="ml-4 mt-3" color="blue darken-4">
-                {{ hotel.star }}
+                {{ restaurant.star }}
                 <v-icon color="yellow darken-2" small>mdi-star</v-icon>
               </v-btn>
             </v-card-title>
             <v-spacer></v-spacer>
-            <v-card-subtitle class="pb-0">{{ hotel.content.substring(0,100) + "..." }}</v-card-subtitle>
+            <v-card-subtitle class="pb-0">{{ restaurant.content.substring(0,100) + "..." }}</v-card-subtitle>
             <v-card-text class="white-text text-right">
-              <div>{{ hotel.location }}</div>
+              <div>{{ restaurant.location }}</div>
             </v-card-text>
           </v-card>
         </v-col>
       </v-row>
-      <v-row justify = "end">
+  <v-row justify = "end">
            <router-link style="text-decoration: none"
-        to="/hotels"
+        to="/restaurants"
       >
      <v-btn align="right" text color="primary">
-      More<v-icon right dark>mdi-bed</v-icon>
+      More<v-icon right dark>mdi-silverware-fork-knife</v-icon>
     </v-btn>
            </router-link>
       </v-row>
@@ -39,17 +39,18 @@
 </template>
 
 <script>
-import API from "../api/hotelapi";
+import API from "../api/restaurantapi";
 
 export default {
-  name: "LatestHotels",
+  name: "LatestRestaurants",
+  
   data() {
     return {
-      hotels: [],
+      restaurants: [],
     };
   },
   async created() {
-    this.hotels = await API.getAllHotels();
+    this.restaurants = await API.getAllRestaurants();
   },
  
 };
