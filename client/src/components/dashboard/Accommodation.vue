59851e1972e6935f2146d1bd70b977ae34aaf65e
Qendresa Berisha <73610620+QendresaBerisha@users.noreply.github.com>
2022-03-07 17:21:37 +0100

    can edit add and delete data but won't save into db

--- client/src/components/dashboard/Accommodation.vue
+++ client/src/components/dashboard/Accommodation.vue
@@ -1,78 +1,96 @@
 <template>
-  <v-card>
-    <v-card-title>
-      <v-toolbar-title>Accommodations</v-toolbar-title>
-      <v-divider class="mx-4" inset vertical></v-divider>
-      <v-dialog v-model="dialog" persistent max-width="600px">
-        <template v-slot:activator="{ on, attrs }">
-          <v-fab-transition>
-            <v-btn color="primary" dark v-bind="attrs" v-on="on">
+  <v-data-table
+    :headers="headers"
+    :items="hotels"
+    :search="search"
+    class="elevation-1"
+    item-key="title"
+    show-expand
+  >
+    <template v-slot:top>
+      <v-toolbar
+        flat
+      >
+        <v-toolbar-title>Accommodations</v-toolbar-title>
+        <v-divider
+          class="mx-4"
+          inset
+          vertical
+        ></v-divider>
+        <v-dialog
+          v-model="dialog"
+          max-width="500px"
+        >
+          <template v-slot:activator="{ on, attrs }">
+            <v-btn
+              color="primary"
+              dark
+              v-bind="attrs"
+              v-on="on"
+            >
               New Item
             </v-btn>
-          </v-fab-transition>
-        </template>
-        <v-form ref="form" @submit="submitForm" enctype="mutipart/form-data">
+          </template>
           <v-card>
             <v-card-title>
-              <span class="text-h5">Add Accommodation</span>
+              <span class="text-h5">{{ formTitle }}</span>
             </v-card-title>
+
             <v-card-text>
               <v-container>
                 <v-row>
-                  <v-col cols="12" sm="6">
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      label="Accommodation Title*"
-                      v-model="hotel.title"
+                      label="Title*"
+                      v-model="editedItem.title"
                       required
-                      :rules="rules"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6">
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      label="Accommodation Category*"
-                      v-model="hotel.category"
+                      label="Category*"
+                      v-model="editedItem.category"
                       required
-                      :rules="rules"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6">
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      label="Accommodation Stars*"
-                      v-model="hotel.star"
+                      label="Stars*"
+                      v-model="editedItem.star"
+                      type="number"
                       required
-                      :rules="rules"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6">
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      label="Accommodation Phone*"
-                      v-model="hotel.number"
+                      label="Phone*"
+                      v-model="editedItem.number"
                       required
-                      :rules="rules"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="12">
+                  <v-col cols="12" sm="6" md="4">
                     <v-textarea
-                      label="Accommodation Description"
-                      v-model="hotel.content"
+                      label="Description"
+                      v-model="editedItem.content"
+                      no-resize
+                      rows="1"
                     >
                     </v-textarea>
                   </v-col>
-                  <v-col cols="12" sm="6">
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      label="Accommodation E-Mail*"
-                      v-model="hotel.email"
+                      label="E-Mail*"
+                      v-model="editedItem.email"
                       required
-                      :rules="emailRules"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6">
+                  <v-col cols="12" sm="6" md="4">
                     <v-text-field
-                      label="Accommodation Website"
-                      v-model="hotel.web"
+                      label="Website"
+                      v-model="editedItem.web"
                     ></v-text-field>
                   </v-col>
-                  <v-col cols="12" sm="6">
+                  <v-col cols="12" sm="6" md="4">
                     <v-select
                       :items="[
                         'Prishtine',
@@ -83,68 +101,71 @@
                       ]"
                       label="Location*"
                       required
-                      v-model="hotel.location"
-                      :rules="rules"
+                      v-model="editedItem.location"
                     ></v-select>
                   </v-col>
-                  <v-col cols="12" sm="6">
+                  <v-col cols="12" sm="6" md="4">
                     <v-file-input
                       @change="selectFile"
                       show-size
                       counter
                       multiple
                       required
-                      :rules="rules"
-                      label="Add Image*"
+                      label="Change Image*"
                       prepend-icon="mdi-file-image-plus"
                     ></v-file-input>
                   </v-col>
                 </v-row>
               </v-container>
-              <small>*indicates required field</small>
             </v-card-text>
+
             <v-card-actions>
               <v-spacer></v-spacer>
-              <v-btn color="red darken-1" text @click="dialog = false">
-                Close
+              <v-btn
+                color="red darken-1"
+                text
+                @click="close"
+              >
+                Cancel
               </v-btn>
               <v-btn
                 color="blue darken-1"
                 text
-                type="submit"
-                @click="dialog = false"
+                @click="save"
               >
                 Save
               </v-btn>
             </v-card-actions>
           </v-card>
-        </v-form>
-      </v-dialog>
-
-      <v-spacer></v-spacer>
-      <v-text-field
+        </v-dialog>
+        <v-spacer></v-spacer>
+              <v-text-field
         v-model="search"
         append-icon="mdi-magnify"
         label="Search"
         single-line
         hide-details
       ></v-text-field>
-    </v-card-title>
-    <v-data-table
-       :headers="headers"
-      :items="hotels"
-      item-key="title"
-      show-expand
-      class="elevation-1"
-    >
+        <v-dialog v-model="dialogDelete" max-width="500px">
+          <v-card>
+            <v-card-title class="text-h5">Are you sure you want to delete this item?</v-card-title>
+            <v-card-actions>
+              <v-spacer></v-spacer>
+              <v-btn color="blue darken-1" text @click="closeDelete">Cancel</v-btn>
+              <v-btn color="blue darken-1" text @click="deleteItemConfirm">OK</v-btn>
+              <v-spacer></v-spacer>
+            </v-card-actions>
+          </v-card>
+        </v-dialog>
+      </v-toolbar>
+    </template>
     <template v-slot:expanded-item="{ headers, item }">
         <td :colspan="headers.length">
           <v-row>
-            <v-col left cols="12" md="8">
+            <v-col cols="12" md="8">
               <h1>{{ item.title }}</h1>
               <v-container>
                 <v-img
-                align="left"
                   class="img"
                   :src="require(`../../../../server/uploads/${item.image}`)"
                   height="200px"
@@ -152,105 +173,108 @@
                 ></v-img>
               </v-container>
             </v-col>
+            <v-col cols="12" sm="6" md="4">
+              <v-card flat>
+                <v-card-title class="text-center"> Description </v-card-title>
+                <v-card-subtitle>
+                  {{ item.content.substring(0, 250) + "..." }}
+                </v-card-subtitle>
+              </v-card>
+            </v-col>
           </v-row>
         </td>
       </template>
-      <template v-slot:[`item.action`]="{ item }">
-         <v-btn 
-              color="primary" 
-              text
-              small
-              :to="{ name: 'hotelDetails', params: { id: item._id } }"
-              > Details
-               </v-btn>
-       <!-- <v-icon small color="blue darken-1" class="mr-2"> mdi-pencil </v-icon>
-        <v-dialog v-model="deleteDialog" persistent max-width="440">
-          <template v-slot:activator="{ on, attrs }">
-            <v-icon small color="red darken-1" v-bind="attrs" v-on="on">
-              mdi-delete
-            </v-icon>
-          </template>
-          <v-card>
-            <v-card-title class="text-h5">
-              Are you sure you want to delete this accommodation?
-            </v-card-title>
-            <v-card-text color="red darken-1"
-              >This action cannot be reversed!</v-card-text
-            >
-            <v-card-actions>
-              <v-spacer></v-spacer>
-              <v-btn color="blue darken-1" text @click="deleteDialog = false">
-                Cancel
-              </v-btn>
-              <v-btn color="red darken-1" text @click="removeHotel( { id: item._id })">
-                Delete
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-dialog>-->
-      </template>
-    </v-data-table>
-  </v-card>
+      <template v-slot:[`item.actions`]="{ item }">
+      <v-icon
+        small
+        color="blue darken-1"
+        class="mr-2"
+        @click="editItem(item)"
+      >
+        mdi-pencil
+      </v-icon>
+      <v-icon
+        small
+        color="red darken-1"
+        @click="deleteItem(item)"
+      >
+        mdi-delete
+      </v-icon>
+    </template>
+  </v-data-table>
 </template>
 
 <script>
 import API from "../../api/hotelapi";
-import HotelDetails from "../../views/HotelDetails";
-export default {
-  name: "Hotel",
-  components: {
-    HotelDetails,
-  },
-  data() {
-    return {
+
+  export default {
+    data: () => ({
       search: "",
+      dialog: false,
+      dialogDelete: false,
       headers: [
         {
-          text: "Name",
-          align: "start",
-          value: "title",
+          text: 'Name',
+          align: 'start',
+          value: 'title',
         },
-        { text: "Category", value: "category" },
-        { text: "Location", value: "location" },
-        { text: "Created", value: "created" },
-        { text: "Actions", value: "action", sortable: false },
+        { text: 'Category', value: 'category' },
+        { text: 'Location', value: 'location' },
+        { text: 'Created', value: 'created' },
+        { text: 'Actions', value: 'actions', sortable: false },
       ],
       hotels: [],
-      rules: [(value) => !!value || "This field is required."],
-      emailRules: [
-        (v) => !!v || "E-mail is required",
-        (v) => /.+@.+/.test(v) || "E-mail must be valid",
-      ],
-      deleteDialog: false,
-      dialog: false,
-      hotel: {
-        title: "",
-        category: "",
-        star: "",
-        number: "",
-        content: "",
-        email: "",
-        web: "",
-        location: "",
-        image: "",
+      editedIndex: -1,
+      editedItem: {
+        title: '',
+        category: '',
+        star: 0,
+        number: 0,
+        content: '',
+        email: '',
+        web: '',
+        location: '',
+        image: '',
       },
-      image: "",
-    };
-  },
-  async created() {
+      image: '',
+      hotel: {
+        title: '',
+        category: '',
+        star: 0,
+        number: 0,
+        content: '',
+        email: '',
+        web: '',
+        location: '',
+        image: '',
+      },
+      image: '',
+    }),
+
+    computed: {
+      formTitle () {
+        return this.editedIndex === -1 ? 'New Item' : 'Edit Item'
+      },
+    },
+
+    watch: {
+      dialog (val) {
+        val || this.close()
+      },
+      dialogDelete (val) {
+        val || this.closeDelete()
+      },
+    },
+
+    created () {
+    },
+  async mounted() {
     this.hotels = await API.getAllHotels();
   },
-  methods: {
-    
-      goToHotel(item){
-        this.$router.push(`/hotel/${item.id}`)
-      },
-    
-    selectFile(file) {
+
+    methods: {
+     selectFile(file) {
       this.image = file[0];
-    },
-    async removeHotel(item) {
-      const response = await API.deleteHotel(item.id);
     },
     async submitForm() {
       const formData = new FormData();
@@ -265,9 +289,51 @@ export default {
       formData.append("location", this.hotel.location);
       if (this.$refs.form.validate()) {
         const response = await API.addHotel(formData);
-        this.$router.push({ name: "hotels" });
+        this.$router.push({ name: "Dashboard" });
       }
     },
-  },
-};
+      
+      editItem (item) {
+        this.editedIndex = this.hotels.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialog = true
+      },
+
+      deleteItem (item) {
+        this.editedIndex = this.hotels.indexOf(item)
+        this.editedItem = Object.assign({}, item)
+        this.dialogDelete = true
+      },
+
+      deleteItemConfirm () {
+        this.hotels.splice(this.editedIndex, 1)
+        this.closeDelete()
+      },
+
+      close () {
+        this.dialog = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      closeDelete () {
+        this.dialogDelete = false
+        this.$nextTick(() => {
+          this.editedItem = Object.assign({}, this.defaultItem)
+          this.editedIndex = -1
+        })
+      },
+
+      save () {
+        if (this.editedIndex > -1) {
+          Object.assign(this.hotels[this.editedIndex], this.editedItem)
+        } else {
+          this.hotels.push(this.editedItem)
+        }
+        this.close()
+      },
+    },
+  }
 </script>
